e08f360f438f62560ddd8036a7851caaad91f26f
Louis Rex <rlhardrock@gmail.com>
2020-12-15 10:31:03 -0500

    creacion models y  migrations Art,Cat,Usu

---
+++ migrations/20201215151253-create-articulo.js
@@ -0,0 +1,50 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Articulos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      codigo: {
+        type: Sequelize.STRING
+      },
+      nombre: {
+        type: Sequelize.STRING
+      },
+      descripcion: {
+        type: Sequelize.STRING
+      },
+      precio_venta: {
+        type: Sequelize.INTEGER
+      },
+      stock: {
+        type: Sequelize.INTEGER
+      },
+      estado: {
+        type: Sequelize.INTEGER
+      },
+      categoriaId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Categoria',
+          key: 'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Articulos');
+  }
+};
